b35aa7cc00347974e9653bbbe568946d37d6e21e
PooMi56 <poowanai.char@sbac.ac.th>
2023-06-14 10:43:04 +0700

    Add project files.

---
+++ Loop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int iterator = 0; iterator < 3; iterator++)
+            {
+                Console.WriteLine("Outer Loop :" + iterator);
+
+                for (int _iterator = 0; _iterator < 2; ++_iterator)
+                {
+                    Console.WriteLine("Inner Loop : " + iterator);
+                }
+            }
+
+            string[] brandTV = { "LG", "SONY", "SAMSUNG", "TCL", "JVC" };
+
+            foreach (string television in brandTV)
+            {
+                Console.WriteLine("Brand" + television);
+            }
+        }
+    }
+}
+
+
+
+
+
